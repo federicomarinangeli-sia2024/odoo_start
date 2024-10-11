f5450f740c869033bc5e4ebab4cb8e6d9dc617e7
Federico Marinangeli <marinangeli.1966821@uniroma1.it>
2024-10-11 17:28:44 +0200

    prdctcust aggiornam per error

--- addons/product_custom/__manifest__.py
+++ addons/product_custom/__manifest__.py
@@ -9,8 +9,7 @@
     'author': 'Il tuo nome',
     'depends': ['product'],  # Dipendenza dal modulo 'product'
     'data': [
-        'views/product_template_view.xml',  # Include il file XML per le viste
-        'views/product_template_search_view.xml',  # Include il file XML per i filtri di ricerca
+        'views/product_template_view.xml',  # Include la vista form e i filtri di ricerca
     ],
     'installable': True,
     'auto_install': False,
