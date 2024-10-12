6bc087361cfcaa218888494271854a22eae26a07
Federico Marinangeli <marinangeli.1966821@uniroma1.it>
2024-10-12 15:17:53 +0200

    modif prodctcodt errore

--- addons/product_custom/__init__.py
+++ addons/product_custom/__init__.py
@@ -1,3 +1,14 @@
-# __init__.py
 from . import models
 
+def post_init_check(cr, registry):
+    """Controlla se il modulo è installato correttamente."""
+    from odoo import api, SUPERUSER_ID
+    import logging
+
+    _logger = logging.getLogger(__name__)
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    if not env['ir.model'].search([('model', '=', 'product.template')]):
+        _logger.error("Errore durante il caricamento del modulo: il modello 'product.template' non è stato trovato.")
+    else:
+        _logger.info("Il modulo 'product.template' è stato caricato correttamente.")
--- addons/product_custom/__manifest__.py
+++ addons/product_custom/__manifest__.py
@@ -10,22 +10,11 @@
     'author': 'Il tuo nome',
     'depends': ['product'],  # Dipendenza dal modulo 'product'
     'data': [
-        # Viste
         'views/product_template_view.xml',  # Include la vista form e i filtri di ricerca
     ],
-    # Aggiunta di controlli extra per assicurare che il modulo funzioni correttamente
     'installable': True,
     'application': False,
     'auto_install': False,
     'license': 'LGPL-3',
-    'post_init_hook': 'post_init_check',  # Hook per verificare che il modulo si sia caricato correttamente
+    'post_init_hook': 'post_init_check',  # Richiamo del post_init_hook senza definizione della funzione qui
 }
-
-# Aggiungi questo nel file `__init__.py` del modulo per definire il post_init_hook:
-def post_init_check(cr, registry):
-    """Controlla se il modulo è installato correttamente."""
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    if not env['ir.model'].search([('model', '=', 'product.template')]):
-        _logger.error("Errore durante il caricamento del modulo: il modello 'product.template' non è stato trovato.")
-    else:
-        _logger.info("Il modulo 'product.template' è stato caricato correttamente.")
